452354f22afc502563dd91bf6c697a751cbc8782
Нартай Дембаев <mastaok02@gmail.com>
2019-06-14 14:23:59 +0600

    fix second app

---
+++ packages/comp-text/src/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Text extends Component {
+    render() {
+        return (
+            <span>Text</span>
+        )
+    }
+}
+
+export default Text;
