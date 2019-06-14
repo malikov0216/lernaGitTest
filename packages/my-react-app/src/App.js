441db574b50db361ae1b09a7a80dc68ca5a4c3c8
Нартай Дембаев <mastaok02@gmail.com>
2019-06-14 11:56:02 +0600

    git test lerna

---
+++ packages/my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from '@project/comp-button'
+
+function App() {
+  return (
+    <div className="App">
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
